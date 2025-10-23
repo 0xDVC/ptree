473a9480ea0af97a6d415f5af187588ba67d59ca
0xdvc <neilohene@gmail.com>
2025-10-23 21:29:45 +0000

    able to scan pid and do basic parsing

--- main.go
+++ main.go
@@ -1,27 +1,46 @@
 package main
 
-// idea is to create a process memory attribution tool,
-// where we can see the memory attribution of a process
-
 import (
-	"fmt"
-	"os/exec"
-	"strings"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	cmd:= exec.Command("ps", "-ax", "-o", "pid")
-	output, err:= cmd.Output()
-	if err != nil {
-		fmt.Println("error running ps command:", err)
-		return
-	}
+    entries, _ := os.ReadDir("/proc")
 
-	lines := strings.Split(string(output), "\n")
-	for _, line:= range lines {
-		line = strings.TrimSpace(line)
-		if line!= "" && len(line) > 0 && line[0] >= '0' && line[0] <= '9' {
-			fmt.Println(line)
-		}
-	}
+    for _, entry := range entries {
+        pid:= entry.Name()
+
+        // see if this is a PID directory?
+        if _, err := strconv.Atoi(entry.Name()); err != nil {
+                continue
+        }
+
+        // read the status file
+        data,err := os.ReadFile("/proc/" +pid+ "/status")
+        if err != nil {
+                continue
+        }
+
+        //parse it
+        lines:= strings.Split(string(data), "\n")
+        var ppid, rss, name string
+
+        for _, line:= range lines {
+                if strings.HasPrefix(line, "PPid:") {
+                        ppid= strings.Fields(line)[1]
+                }
+                if strings.HasPrefix(line, "VmRSS:") {
+                        rss = strings.Fields(line)[1]
+                }
+                if strings.HasPrefix(line, "Name:") {
+                        name = strings.Fields(line)[1]
+                }
+        }
+
+        fmt.Printf("pid:  %s, ppid: %s, rss: %s KB, name: %s\n", pid, ppid, rss, name)
+
+    }
 }
--- readme.md
+++ readme.md
@@ -14,8 +14,34 @@
 <h1 class='title'>ptree</h1>
 <span>trying to build a simple process memory attribution tool before dawn. lets see how it goes.</span>
 
-<h3>Resource</h3>
+<h3>progress</h3>
+**should be changing as i proceed
+<pre>
+pid:  1, ppid: 0, rss: 184 KB, name: init
+pid:  1900, ppid: 479, rss: 21136 KB, name: go
+pid:  1989, ppid: 1900, rss: 2392 KB, name: main
+pid:  276, ppid: 1, rss: 212 KB, name: syslogd
+pid:  304, ppid: 1, rss: 228 KB, name: crond
+pid:  403, ppid: 1, rss: 168 KB, name: udhcpc
+pid:  467, ppid: 1, rss: 180 KB, name: getty
+pid:  479, ppid: 59, rss: 896 KB, name: sh
+pid:  59, ppid: 1, rss: 10144 KB, name: orbstack-agent:
+</pre>
+
+<h3>testing</h3>
+<em>disclaimer:</em> i am running this on my homelab(alpine 3.22, arm64 on orbstack vm). this should work on any linux distro.
+
+<h3>requirements</h3>
+<ul>
+<li>any linux distro</li>
+<li>>=go1.21</li>
+</ul>
+
+<h3>resource</h3>
 <ul>
     <li>
         <a href="https://broman.dev/download/The%20Linux%20Programming%20Interface.pdf">The Linux Programming book, Michael Kerrisk</a>
     </li>
+</ul>
+
+<em>ET: 30m</em>
